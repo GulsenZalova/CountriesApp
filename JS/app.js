48a776fcb8833e163faf37b35d387713f3e84ec2
GulsenZalova <zalova0202@gmail.com>
2022-12-11 02:17:52 +0400

    Filtered,Borders,Languages Completely  Done

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,5 @@
 let countriesContainer = document.querySelector(".countries .container ")
-let countryDetailes=document.querySelector(".countryDetailes .container")
+let countryDetailes = document.querySelector(".countryDetailes .container")
 let filterNameInput = document.querySelector(".filterNameInput")
 let selectRegions = document.querySelector("#selectRegions")
 
@@ -10,34 +10,62 @@ function getCountryInfo() {
 }
 
 function displayCountryInfo(infos) {
+
     let countryInfo = ""
-    infos.forEach((info,i)=> {
+    infos.forEach((info, i) => {
         countryInfo += ` 
-<div class="country">
-<a class="country-image" onclick=moreDetails(${i})>
-    <img src="${info.flags.svg}" class="country-img" alt="">
-</a>
-<div class="country-info">
-    <div><span class="country-name">${info.name.common}</span></div>
-    <div>Population: <span class="country-population">${info.population}</span></div>
-    <div>Region: <span class="country-region">${info.region}</span></div>
-    <div>Capital: <span class="country-capital">${info.capital}</span></div>
-</div>
-</div> 
-        `
+    <div class="country">
+    <a class="country-image" onclick=moreDetails(${i})>
+        <img src="${info.flags.svg}" class="country-img" alt="">
+    </a>
+    <div class="country-info">
+        <div><span class="country-name">${info.name.common}</span></div>
+        <div>Population: <span class="country-population">${info.population}</span></div>
+        <div>Region: <span class="country-region">${info.region}</span></div>
+        <div>Capital: <span class="country-capital">${info.capital}</span></div>
+    </div>
+    </div> 
+            `
         countriesContainer.innerHTML = countryInfo
     });
 }
 
-filterNameInput.addEventListener("keyup", function () {
+filterNameInput.addEventListener("input", function () {
     let searchName = filterNameInput.value
+    if(searchName.length>0){
     fetch(`https://restcountries.com/v3.1/name/${searchName}`)
         .then(res => res.json())
         .then(data => {
-            displayCountryInfo(data)
-        })
-})
+            if (!data.length) {
+                countriesContainer.innerHTML = `<div class="errorContainer"><span class="errorContent">No İnformation Found</span></div>`
+            } else {
+                getData()
+            }
 
+        })
+    }else{
+        getCountryInfo()
+    }
+})
+ function getData (){
+    countriesContainer.innerHTML = "";
+    fetch(`https://restcountries.com/v3.1/all`)
+        .then(res => res.json())
+        .then(data => {
+            let filteredCountryList = [];
+            data.forEach(function (element) {
+                if (filterNameInput.value.length> 0) {
+                    let filterName = filterNameInput.value;
+                    if (element.name.common.toLowerCase().includes(filterName.toLowerCase())) {
+                        filteredCountryList.push(element);
+                    }
+                } else {
+                    filteredCountryList = data;
+                }
+            })
+            displayCountryInfo(filteredCountryList);
+        })
+}
 async function getRegions() {
     let resgions = []
     await fetch("https://restcountries.com/v3.1/all")
@@ -75,82 +103,12 @@ displayRegions()
 getCountryInfo()
 
 
-function moreDetails(index){
+function moreDetails(index) {
     fetch("https://restcountries.com/v3.1/all")
-    .then(res => res.json())
-    .then(data =>{
-    displayCountryMoreInfo(data[index].name.common)
-    console.log(data[index].name.common)
-    localStorage.setItem("countryName",JSON.stringify(data[index].name.common))    
-    window.location="./description.html" 
-})
+        .then(res => res.json())
+        .then(data => {
+            console.log(data[index].name.common)
+            localStorage.setItem("countryName", JSON.stringify(data[index].name.common))
+            window.location = "./description.html"
+        })
 }
-
-function displayCountryMoreInfo(country){
-
-    let countryName=country
-    let newCountryInfo=""
-    fetch(`https://restcountries.com/v3.1/name/${countryName}`)
-    .then(res=>res.json())
-    .then(data=>{
-        
-        let name=data[0].name.common
-        let nativeName=Object.entries(data[0].name.nativeName)[0][0]
-        let population=data[0].population
-        let region=data[0].region
-        let subRegion=data[0].subregion
-        let capital=data[0].capital[0]
-        let currencies=Object.entries(data[0].currencies)[0][0]
-        let languages=Object.entries(data[0].languages)[0]
-        let borders=data[0].borders
-
-        newCountryInfo+=`
-
-        <div class="countryInfos">
-        <div class="country-image">
-            <img src="${data[0].flags.svg}" alt="">
-        </div>
-        <div class="country-info">
-            <div class="countryName">${name}</div>
-            <div class="country-details">
-                <div>
-                    <div>Native Name: <span class="nativeName">${nativeName}</span></div>
-                    <div>Population: <span class="population">${population}</span></div>
-                    <div>Region: <span class="region">${region}</span></div>
-                    <div>Sub Region: <span class="subRegion">${subRegion}</span></div>
-                </div>
-                <div>
-                    <div>Capital: <span class="capitalname">${capital}</span></div>
-                    <div>Currencies: <span class="currencies">${currencies}</span></div>
-                    <div>Languages: bla, bla, bla</div>
-                </div>
-            </div>
-            <div class="bordersDiv">
-                <div>
-                    Border Countries:
-                </div>
-                <div class="borders-btns">
-                    <button class="btn">$</button>
-                </div>
-            </div>
-        </div>
-    </div>
-        `
-        console.log(name)
-        countryDetailes.appendChild(newCountryInfo)
-    })
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
--- JS/desc.js
+++ JS/desc.js
@@ -15,7 +15,7 @@ function displayCountryMoreInfo(nameCount) {
         .then(res => res.json())
         .then(data => {
             console.log(data)
-            let name = data[0].name.common
+            let name = data.name
             let nativeName = Object.entries(data[0].name.nativeName)[0][0]
             let population = data[0].population
             let region = data[0].region
@@ -24,7 +24,7 @@ function displayCountryMoreInfo(nameCount) {
             let currencies = Object.entries(data[0].currencies)[0][0]
             let languages = Object.entries(data[0].languages)[0]
             let borders = data[0].borders
-
+            console.log(borders)
             newCountryInfo += `
         <a class="btn-back">Back</a>
         <div class="countryInfos">
@@ -32,45 +32,66 @@ function displayCountryMoreInfo(nameCount) {
             <img src="${data[0].flags.svg}" alt="">
         </div>
         <div class="country-info">
-            <div class="countryName">${name}</div>
+            <div class="countryName">${countryName ? countryName : ""}</div>
             <div class="country-details">
                 <div>
-                    <div>Native Name: <span class="nativeName">${nativeName}</span></div>
-                    <div>Population: <span class="population">${population}</span></div>
-                    <div>Region: <span class="region">${region}</span></div>
-                    <div>Sub Region: <span class="subRegion">${subRegion}</span></div>
+                    <div>Native Name: <span class="nativeName">${nativeName ? nativeName : ""}</span></div>
+                    <div>Population: <span class="population">${population ? population : ""}</span></div>
+                    <div>Region: <span class="region">${region ? region : ""}</span></div>
+                    <div>Sub Region: <span class="subRegion">${subRegion ? subRegion :""}</span></div>
                 </div>
                 <div>
-                    <div>Capital: <span class="capitalname">${capital}</span></div>
-                    <div>Currencies: <span class="currencies">${currencies}</span></div>
-                    <div>Languages: bla, bla, bla</div>
+                    <div>Capital: <span class="capitalname">${capital ? capital : ""}</span></div>
+                    <div>Currencies: <span class="currencies">${currencies ? currencies : ""}</span></div>
+                    <div class="languages">
+
+                    </div>
                 </div>
             </div>
             <div class="bordersDiv">
-            <div>
-            Border Countries:
-            </div>
             </div>
         </div>
     </div>
         `
-            let bordersList = ""
-            borders.forEach(element => {
-                bordersList += `
+            let bordersList = "Border Countries:"
+            if(borders){
+                borders.forEach(element => {
+                    bordersList += `
+                    <div class="borders-btns">
+                        <button class="btn btn-border">${element}</button>
+                    </div>
+            `
+                });
+            }else{
+                bordersList+=` This country has no borders`
+            }
+            let languagesList="Languages: "
+            languages.forEach((language)=>{
+                languagesList+= `
+                <span class="btn"> ${language}</span> 
+                `
+            })
 
-                <div class="borders-btns">
-                    <button class="btn">${element}</button>
-                </div>
-        `
-            });
-            console.log(borders)
             countryDetailes.innerHTML = newCountryInfo
             let bordersDiv=document.querySelector(".bordersDiv")
+            let languagesDiv=document.querySelector(".languages")
+            let backBTN = document.querySelector(".btn-back") 
+            languagesDiv.innerHTML=languagesList
             bordersDiv.innerHTML=bordersList
-            let backBTN = document.querySelector(".btn-back")
             backBTN.addEventListener("click", function () {
                 window.location = "./index.html"
             })
+
+            let btnBorders=document.querySelectorAll(".btn-border")
+            btnBorders.forEach((btnBorder)=>{
+                btnBorder.addEventListener("click",function(){
+                  let borderCountry=btnBorder.innerHTML
+                  fetch(`https://restcountries.com/v3.1/name/${borderCountry}`)
+                  .then(res=>res.json())
+                  .then(data=>displayCountryMoreInfo(borderCountry))
+                })
+            })
+
         })
 }
 
